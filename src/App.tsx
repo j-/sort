8e39c29c11fed764c96c2e2397985a71ac0860ce
j- <j@skeoh.com>
2018-11-28 11:26:16 +1100

    Add Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 const App: React.StatelessComponent = () => (
-	<div className="App">
-		{null}
+	<div className="App container mt-5 mb-5">
+		<h1 className="mt-5 mb-5">Sort</h1>
 	</div>
 );
 
