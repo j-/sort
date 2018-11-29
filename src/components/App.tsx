a04aedd0ff4c75f8dc1f52ba0fc147359d35eb1b
j- <j@skeoh.com>
2018-11-29 19:26:08 +1100

    Add subheadings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,18 +9,22 @@ const App: React.StatelessComponent = () => (
 		<h1 className="mt-5 mb-5">Sort</h1>
 
 		<section className="mt-5 mb-5">
+			<h2 className="mt-5 mb-5">List to sort</h2>
 			<UnsortedList />
 		</section>
 
 		<section className="mt-5 mb-5">
+			<h2 className="mt-5 mb-5">Prompt</h2>
 			<PromptInput className="form-control" />
 		</section>
 
 		<section className="mt-5 mb-5">
+			<h2 className="mt-5 mb-5">Comparisons</h2>
 			<ComparisonTable />
 		</section>
 
 		<section className="mt-5 mb-5">
+			<h2 className="mt-5 mb-5">Decisions</h2>
 			<ComparisonSummaryList />
 		</section>
 
